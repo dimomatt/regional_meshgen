b3ac2614234eb03fd2506b60b8b38051033f4571
Matthew Dimond <mdimond@cheyenne2.cheyenne.ucar.edu>
2023-04-26 08:12:23 -0600

    tests actually run now

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,2 +1,25 @@
-#include <catch2/catch_test_macros.hpp>
-#include "test_regular_mesh.cpp"
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include <catch2/catch_all.hpp>
+#include "../include/RegularMesh.hpp"
+TEST_CASE("RegularMesh generates cells with correct size", "[RegularMesh]") {
+  RegularMesh mesh(3, 3, 1.0);
+  mesh.generateCells();
+  REQUIRE(mesh.cellsOnPlane.size() == 9);
+}
+
+TEST_CASE("RegularMesh triangulates", "[RegularMesh]") {
+  RegularMesh mesh(3, 3, 1.0);
+  mesh.generateCells();
+  mesh.triangulate();
+  REQUIRE(mesh.vertices.size() > 0);
+  REQUIRE(mesh.edges.size() > 0);
+}
+
+TEST_CASE("RegularMesh generates Voronoi diagram", "[RegularMesh]") {
+  RegularMesh mesh(3, 3, 1.0);
+  mesh.generateCells();
+  mesh.triangulate();
+  mesh.generateVoronoi();
+}
+
